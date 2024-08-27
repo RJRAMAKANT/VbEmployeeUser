9df3c39bee090ad948c86da104c89f74db70958b
RAMAKANTA GAHAN <94637520+RJRAMAKANT@users.noreply.github.com>
2024-08-27 11:26:17 +0530

    Add files via upload

---
+++ EmployeeManageMentSystem/assets/css/js/func.js
@@ -0,0 +1,20 @@
+﻿function modalclick() {
+    var txt_useremail = document.getElementById('forgotemail');
+
+    if (txt_useremail.value == '') {
+        Lobibox.notify('error', {
+            pauseDelayOnHover: true,
+            continueDelayOnInactiveTab: false,
+            // size: 'ini',
+            rounded: true,
+            icon: 'bx bx-x-circle',
+            delayIndicator: true,
+            continueDelayOnInactiveTab: false,
+            position: 'top right',
+            msg: 'Registered EMail Id Can not be Empty'
+        });
+        console.log("user empty");
+        txt_useremail.focus();
+        return false;
+    }
+}
